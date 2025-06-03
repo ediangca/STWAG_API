415f7869a7288aaac189796052addefb09d71256
ediangca22 <ediangca22@gmail.com>
2025-06-03 13:21:51 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,13 +4,14 @@ php artisan config:cache
 # Run migrations
 php artisan migrate --force
 
-# Run spinning algorithm every 60 seconds
-while true; do
-  php artisan spin:run
-  sleep 60
-done
 
 # Start Laravel server in the background
 php artisan serve --host=0.0.0.0 --port=8080 &
 
 # while true; do php artisan spin:run; sleep 60; done
+
+# Run spinning algorithm every 60 seconds
+while true; do
+  php artisan spin:run
+  sleep 60
+done
