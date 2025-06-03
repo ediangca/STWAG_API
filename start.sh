b71b3538e88513e2e198a476b9a393247ba815a7
ediangca22 <ediangca22@gmail.com>
2025-06-03 11:05:20 +0800

    Update Docker, env and start sh

--- start.sh
+++ start.sh
@@ -3,11 +3,13 @@
 # Run migrations
 php artisan migrate --force
 
-# Start Laravel server in the background
-php artisan serve --host=0.0.0.0 --port=10000 &
-
 # Run spinning algorithm every 60 seconds
 while true; do
   php artisan spin:run
   sleep 60
 done
+
+# Start Laravel server in the background
+php artisan serve --host=0.0.0.0 --port=10000 &
+
+# while true; do php artisan spin:run; sleep 60; done
