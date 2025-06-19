a7cab807a39917a83178bbd33fe2be408d7ddaab
ediangca22 <ediangca22@gmail.com>
2025-06-19 16:24:40 +0800

    Update CSRF token handling in reset password view and middleware for improved security

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,9 +12,9 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        // 'api/*', // exclude all API routes
+        'api/*', // exclude all API routes
 
-        'api/auth/reset-password', // Exact route
+        // 'api/auth/reset-password', // Exact route
         // Another specific route
         // 'api/user/profile/update', 
     ];
