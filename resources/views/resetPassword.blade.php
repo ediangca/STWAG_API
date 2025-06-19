5fc95fb69db7ffb878cf1a24eaa12ea0965aee24
ediangca22 <ediangca22@gmail.com>
2025-06-19 13:01:20 +0800

    Remove alert messages from reset password flow for cleaner user experience

--- resources/views/resetPassword.blade.php
+++ resources/views/resetPassword.blade.php
@@ -205,9 +205,9 @@
                         });
                     });
 
-                    alert(
-                        'Please check your email for the password reset link. If you did not receive an email, please check your spam folder or contact support.'
-                    );
+                    // alert(
+                    //     'Please check your email for the password reset link. If you did not receive an email, please check your spam folder or contact support.'
+                    // );
                     // Handle form submission
                     const form = document.getElementById('resetPasswordForm');
                     const greetings = document.getElementById('greetings');
@@ -245,9 +245,9 @@
                             password_confirmation: passwordConfirmation
                         };
 
-                        alert(
-                            'Please wait while we process your request. This may take a few seconds.'
-                        );
+                        // alert(
+                        //     'Please wait while we process your request. This may take a few seconds.'
+                        // );
 
 
                         // The code below is unrelated to your Blade file's password reset logic.
