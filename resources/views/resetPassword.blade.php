2671a2479dcaffa9c397a5b167117c9730318a6f
ediangca22 <ediangca22@gmail.com>
2025-06-20 14:54:09 +0800

    Remove unnecessary script tag from the head section in reset password Blade file

--- resources/views/resetPassword.blade.php
+++ resources/views/resetPassword.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    </script>
     <link rel="icon" type="image/png" href="{{ asset('img/stwag-logo.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
