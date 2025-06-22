a5c0119881893cb74d003fa4c5e98fca9d8d0cea
ediangca22 <ediangca22@gmail.com>
2025-06-22 18:09:00 +0800

    Fix fetch request in resetPassword view to include credentials for CORS

--- resources/views/resetPassword.blade.php
+++ resources/views/resetPassword.blade.php
@@ -257,7 +257,7 @@
                                     'Accept': 'application/json',
                                     'Content-Type': 'application/json',
                                 },
-                                credentials: 'include'
+                                credentials: 'include',
                                 // withCredentials: true // send cookies/CSRF if same origin
                                 body: JSON.stringify(payload),
                             })
