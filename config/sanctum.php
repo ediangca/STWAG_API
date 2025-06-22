55af1d95a4c8b0f509686702400936f00448cf0b
ediangca22 <ediangca22@gmail.com>
2025-06-22 19:03:10 +0800

    Enable withCredentials in resetPassword fetch request to support CORS and CSRF

--- config/sanctum.php
+++ config/sanctum.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\ValidateCsrfToken;
 use Laravel\Sanctum\Sanctum;
 
 return [
@@ -85,7 +86,8 @@ return [
     'middleware' => [
         'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
         'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
-        'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+        // 'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+        'validate_csrf_token' => ValidateCsrfToken::class,
     ],
 
 ];
