7a4428e47d3518349bb6d19b1842198c1e66520c
ediangca22 <ediangca22@gmail.com>
2025-06-23 11:09:32 +0800

    Add Bootstrap CSS link to customMail view for improved styling

--- resources/views/customMail.blade.php
+++ resources/views/customMail.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="description" content="STWAG - Spin to Win and Gain">
     <link rel="icon" type="image/png" href="{{ asset('img/stwaglogo.png') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
