7321c31bc564c3bca522a62598bf9df029315e2f
ediangca22 <ediangca22@gmail.com>
2025-08-03 00:03:16 +0800

    Update custom Email.

--- resources/views/customMail.blade.php
+++ resources/views/customMail.blade.php
@@ -9,10 +9,10 @@
     <link rel="icon" type="image/png" href="{{ asset('img/stwaglogo.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
+    {{-- <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-        rel="stylesheet">
+        rel="stylesheet"> --}}
 
     <title>STWAG</title>
     <style>
